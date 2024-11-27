c3bb72d7c74c5e206ca6f92850c69141680e8e96
AnaTovar-Arg <tovar12kari@gmail.com>
2024-11-27 21:36:37 +0100

    create test and coverage

---
+++ src/test/java/dev/exercise/DniViewTest.java
@@ -0,0 +1,41 @@
+package dev.exercise;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import View.DniView;
+
+public class DniViewTest {
+
+    private ByteArrayInputStream testInput;
+    private ByteArrayOutputStream testOutput;
+    private DniView view;
+    @BeforeEach
+    public void setUp() {
+        testOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOutput));
+        view = new DniView();
+    }
+    @AfterEach
+    public void tearDown() {
+        System.setIn(System.in);
+        System.setOut(System.out);
+    }
+    @Test
+    public void testDisplayDniLetter() {
+        view.displayDniLetter('A');
+        assertTrue(testOutput.toString().contains("The letter corresponding to your DNI is: A"));
+    }
+    @Test
+    public void testDisplayError() {
+        view.displayError("Test error message");
+        assertTrue(testOutput.toString().contains("Test error message"));
+    }
+}
+
