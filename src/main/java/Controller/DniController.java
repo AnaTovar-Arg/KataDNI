b6b9c964dcd7195cd61fa1b810ea896bde02e1d0
AnaTovar-Arg <tovar12kari@gmail.com>
2024-11-27 21:18:20 +0100

    create model view and controller

--- src/main/java/Controller/DniController.java
+++ src/main/java/Controller/DniController.java
@@ -1,5 +1,29 @@
 package Controller;
 
-public class DniController {
+import Model.DniCalculator;
+import View.DniView;
 
+public class DniController {   
+
+    private final DniCalculator model;
+    private final DniView view;
+    public DniController(DniCalculator model, DniView view) {
+        this.model = model;
+        this.view = view;
+    }
+    public void run() {
+        while (true) {
+            try {
+                int dniNumber = view.getDniInput();
+                char letter = model.calculateDniLetter(dniNumber);
+                view.displayDniLetter(letter);
+                break;
+            } catch (IllegalArgumentException e) {
+                view.displayError(e.getMessage());
+            }
+        }
+        view.close();
+    }
 }
+    
+
---
+++ src/main/java/Model/DniCalculator.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class DniCalculator {
+
+    private static final char[] DNI_LETTERS = {
+        'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B',
+        'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'
+    };
+    public char calculateDniLetter(int dniNumber) {
+        if (dniNumber < 0 || dniNumber > 99999999) {
+            throw new IllegalArgumentException("DNI number must be between 0 and 99,999,999.");
+        }
+        int index = dniNumber % 23;
+        return DNI_LETTERS[index];
+    }
+}
